7380a05bc78f978e34cbcce5db54e462e46d8b88
NewBisN <izogrik@gmail.com>
2016-02-21 14:39:17 +0300

    k1_turbo <-> enso

---
+++ enso-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/etuline/enso/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/etuline/enso/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/etuline/enso/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/etuline/enso/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/etuline/enso/proprietary/vendor,system/vendor)
